699a6316f61ef224b69a0c614aa5598d6d5c9f72
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2024-05-02 23:17:50 +0300

    add client component

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,15 @@ import styles from "./page.module.css";
 import nextJsCourse from "../../course.json";
 import Lesson from "./lesson.js";
 
+import dynamic from "next/dynamic";
+const ClientComponent = dynamic(() => import("./someClien.js"), {
+  ssr: false,
+});
+
+// const Lesson = dynamic(() => import("./lesson.js"), {
+//   ssr: false,
+// });
+
 async function getAllCourses() {
   return new Promise((resolve) => {
     setTimeout(() => resolve(nextJsCourse), 1000);
@@ -16,6 +25,8 @@ export default async function Home() {
     <main className={styles.main}>
       <h2>{courses.title}</h2>
       <br></br>
+      <h1>React</h1>
+      <ClientComponent />
 
       <ul>
         {courses.lessons.map((lesson) => {
---
+++ src/app/someClien.js
@@ -0,0 +1,6 @@
+"use client";
+
+export default function SomeClien() {
+  console.log("client component");
+  return <p>clien component</p>;
+}
